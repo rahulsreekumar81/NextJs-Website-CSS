272e5d9a18b2097c6690f928463a828963881185
rahulsreekumar81 <rahulsreekumar81@gmail.com>
2022-07-09 22:19:37 +0530

    ✅ : Added button

--- Components/Button/Button.js
+++ Components/Button/Button.js
@@ -1,4 +1,5 @@
+import styles from "../../styles/Button.module.css";
 const Button = ({ button_text, icon }) => {
-  return <button>{button_text}</button>;
+  return <button className={styles.start_btn}>{button_text}</button>;
 };
 export default Button;
