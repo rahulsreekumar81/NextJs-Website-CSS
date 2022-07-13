d1ccd6c0af8b0599102121610d862718877c2d9b
rahulsreekumar81 <rahulsreekumar81@gmail.com>
2022-07-13 20:48:54 +0530

    ♻️ : removed unwanted Imports

--- Layout/Layout.js
+++ Layout/Layout.js
@@ -1,6 +1,5 @@
 import Navbar from "../Components/Navbar/Navbar";
 import Footer from "../Components/Footer/Footer";
-import { Children } from "react";
 
 const Layout = ({ children }) => {
   return (
