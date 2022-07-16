39fd14fa2574b14eb19404cb9f0735a509e5cb17
rahulsreekumar81 <rahulsreekumar81@gmail.com>
2022-07-16 12:11:02 +0530

    ✅ : Added Company profile skeleton

---
+++ Components/CompanyProfile/CompanyProfile.js
@@ -0,0 +1,36 @@
+import styles from "../../styles/CompanyProfile.module.css";
+import Image from "next/image";
+const CompanyProfile = () => {
+  return (
+    <section className={styles.sectionContainer}>
+      <div className={styles.headingContainer}>
+        <h2 className={styles.primaryHeading}>
+          Company Profile<span className={styles.fullStop}>.</span>
+        </h2>
+        <h4 className={styles.secondaryHeading}>Short Introduction</h4>
+      </div>
+      <div className={styles.contentContainer}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/companyimage.png"
+            alt="companyPhoto"
+            height="100%"
+            width="100%"
+          />
+        </div>
+        <div className={styles.content}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default CompanyProfile;
