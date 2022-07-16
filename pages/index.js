ce41a92ae72ad68135661b9e1a1d17d2dc06b028
rahulsreekumar81 <rahulsreekumar81@gmail.com>
2022-07-16 12:13:37 +0530

    ✅ : Added Company Profile

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
 import HeroSection from "../Components/HeroSection/HeroSection";
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <HeroSection />
+      <CompanyProfile />
     </div>
   );
 }
