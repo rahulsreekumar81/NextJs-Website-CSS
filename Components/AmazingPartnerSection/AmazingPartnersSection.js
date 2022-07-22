d81c26457e75d36574e0eb6cc5a9daf5ed67493a
rahulsreekumar81 <rahulsreekumar81@gmail.com>
2022-07-22 22:27:24 +0530

    ✅ : Added Amazng partners section skeleton

---
+++ Components/AmazingPartnerSection/AmazingPartnersSection.js
@@ -0,0 +1,17 @@
+const AmazingPartnersSection = () => {
+  return (
+    <section>
+      <div>
+        <div className="headingContainer">
+          <h2 className="primaryHeading">
+            Amazing Partners<span className="fullStop">.</span>
+          </h2>
+          <h4 className="secondaryHeading">Short Introduction</h4>
+        </div>
+        <div className=""></div>
+      </div>
+    </section>
+  );
+};
+
+export default AmazingPartnersSection;
