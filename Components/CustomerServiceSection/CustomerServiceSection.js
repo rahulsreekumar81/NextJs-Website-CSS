03e8adb0a443ebc000013af876ea89953f2f1278
Rahul <rahulsreekumar81@gmail.com>
2022-07-26 12:25:25 +0530

    🚧 : layout changed

--- Components/CustomerServiceSection/CustomerServiceSection.js
+++ Components/CustomerServiceSection/CustomerServiceSection.js
@@ -1,15 +1,16 @@
 import styles from "../../styles/CustomerService.module.css";
 const CustomerServiceSection = () => {
   return (
-    <section className={styles.paddingBlock900}>
-      <div className="container">
-        <div className={styles.evenColumns}>
-          <div>
-            <h2 className={styles.primaryHeading}>
-              Customer Service<span className={styles.fullStop}>.</span>
-            </h2>
-            <h4 className={styles.secondaryHeading}>Short Introduction</h4>
-          </div>
+    <section className={styles.sectionContainer}>
+      {/* <div className="container"> */}
+      <div className={styles.evenColumns}>
+        <div className={styles.headingContainer}>
+          <h2 className={styles.primaryHeading}>
+            Customer Service<span className={styles.fullStop}>.</span>
+          </h2>
+          <h4 className={styles.secondaryHeading}>Short Introduction</h4>
+        </div>
+        <div>
           <ul className={styles.numberedList}>
             <li className={styles.listItems}>
               <h5 className={styles.listItem}>+1-234-567-890</h5>
@@ -24,10 +25,11 @@ const CustomerServiceSection = () => {
             </li>
           </ul>
         </div>
-        <div>
-          <img src="/mapImage.png" alt="map" className={styles.mapImage} />
-        </div>
       </div>
+      <div>
+        {/* <img src="/mapImage.png" alt="map" className={styles.mapImage} /> */}
+      </div>
+      {/* </div> */}
     </section>
   );
 };
