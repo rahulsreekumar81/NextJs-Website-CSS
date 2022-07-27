509fc9a7ab86265b1049423a7af0ae7fe357b1c9
rahulsreekumar81 <rahulsreekumar81@gmail.com>
2022-07-27 14:32:09 +0530

    🎨 : Improved the skeleton and renamed the classes

--- Components/CustomerServiceSection/CustomerServiceSection.js
+++ Components/CustomerServiceSection/CustomerServiceSection.js
@@ -2,34 +2,34 @@ import styles from "../../styles/CustomerService.module.css";
 const CustomerServiceSection = () => {
   return (
     <section className={styles.sectionContainer}>
-      {/* <div className="container"> */}
-      <div className={styles.evenColumns}>
-        <div className={styles.headingContainer}>
-          <h2 className={styles.primaryHeading}>
-            Customer Service<span className={styles.fullStop}>.</span>
-          </h2>
-          <h4 className={styles.secondaryHeading}>Short Introduction</h4>
+      <div className={styles.flexContainer}>
+        <div className={styles.flexChild1}>
+          <div className={styles.headingContainer}>
+            <h2 className={styles.primaryHeading}>Customer Service</h2>
+            <h4 className={styles.secondaryHeading}>Short Introduction</h4>
+          </div>
+          <div className={styles.listContainer}>
+            <ul className={styles.list}>
+              <li className={styles.listItems}>
+                <h5 className={styles.listItems}>+1-234-567-890</h5>
+              </li>
+              <li className={styles.listItems}>
+                <h5 className={styles.listItems}>
+                  Financial Service@gmail.com
+                </h5>
+              </li>
+              <li className={styles.listItems}>
+                <h5 className={styles.listItems}>
+                  Rockefeller PlazaNew York, NY 10112
+                </h5>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div>
-          <ul className={styles.numberedList}>
-            <li className={styles.listItems}>
-              <h5 className={styles.listItem}>+1-234-567-890</h5>
-            </li>
-            <li className={styles.listItems}>
-              <h5 className={styles.listItem}>Financial Service@gmail.com</h5>
-            </li>
-            <li className={styles.listItems}>
-              <h5 className={styles.listItem}>
-                Rockefeller PlazaNew York, NY 10112
-              </h5>
-            </li>
-          </ul>
+        <div className={styles.flexChild2}>
+          <img src="/mapImage.png" alt="logo" className={styles.image} />
         </div>
       </div>
-      <div>
-        {/* <img src="/mapImage.png" alt="map" className={styles.mapImage} /> */}
-      </div>
-      {/* </div> */}
     </section>
   );
 };
