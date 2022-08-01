f84f335094d680a64b735ea42a50e3736a062a24
Rahul <rahulsreekumar81@gmail.com>
2022-08-01 13:37:05 +0530

    ♻️

--- Components/HeroSection/HeroSection.js
+++ Components/HeroSection/HeroSection.js
@@ -8,12 +8,6 @@ const HeroSection = () => {
           Your strategy is only as good as
           <br /> you execute it<span className={styles.fullStop}>.</span>
         </h1>
-        {/* <Image
-          src="/Cover.png"
-          alt="background"
-          layout="fill"
-          className={styles.imageContainer}
-        /> */}
         <Button button_text="Get Started" className={styles.buttonPrimary} />
       </div>
     </section>
