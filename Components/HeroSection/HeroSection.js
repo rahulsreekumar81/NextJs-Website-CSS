21519ce5bacf0ecc9fe75d72138f07c0466635b6
Rahul <rahulsreekumar81@gmail.com>
2022-08-01 15:33:54 +0530

    💄 : Added background image and gradient

--- Components/HeroSection/HeroSection.js
+++ Components/HeroSection/HeroSection.js
@@ -3,15 +3,14 @@ import styles from "../../styles/HeroSection.module.css";
 const HeroSection = () => {
   return (
     <section className={styles.sectionContainer}>
-      <div className={styles.hero}>
-        <div className={styles.container}>
-          <h1>
-            Your strategy is only as good as
-            <br /> you execute it<span className={styles.fullStop}>.</span>
-          </h1>
-          <Button button_text="Get Started" className={styles.buttonPrimary} />
-        </div>
+      <div className={styles.container}>
+        <h1>
+          Your strategy is only as good as
+          <br /> you execute it<span className={styles.fullStop}>.</span>
+        </h1>
+        <Button button_text="Get Started" className={styles.buttonPrimary} />
       </div>
+      <div className={styles.hero} />
     </section>
   );
 };
